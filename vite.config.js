d03b4ed389a221bd6c23cd1f240cb8438dee3ece
yano1978 <yano1978@gmail.com>
2023-05-11 10:06:41 +0100

    vercel dev attempt reverted

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
   plugins: [svgr(), react()],
   resolve: {
     alias: {
