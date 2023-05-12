c4f17f42168f12f720ce93ef76a45b12c05c4c66
yano1978 <yano1978@gmail.com>
2023-05-12 08:29:50 +0100

    404.html page & config added

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   base: 'https://fallofsaturn.com',
   plugins: [svgr(), react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'public/404.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
