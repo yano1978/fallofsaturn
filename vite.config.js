e0ba6ed07cc3a87a02a596a34997d442c2966af6
yano1978 <yano1978@gmail.com>
2023-05-12 07:55:14 +0100

    https added to base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://fallofsaturn.com',
+  base: 'https://fallofsaturn.com',
   plugins: [svgr(), react()],
   resolve: {
     alias: {
