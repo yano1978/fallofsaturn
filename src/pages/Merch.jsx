7a0dbfe741aac129adf32a3216e03c122fb220cf
yano1978 <yano1978@gmail.com>
2024-04-29 23:44:27 +0100

    Small CSS fix

--- src/pages/Merch.jsx
+++ src/pages/Merch.jsx
@@ -6,11 +6,11 @@ const Merch = () => {
     <>
       <div className="absolute left-0 top-0 flex h-screen w-screen flex-col items-center justify-center p-10">
         <h2 className="z-40 my-5 font-hypo text-[46px] font-bold">MERCH</h2>
-        <div className="z-40 mx-20 grid gap-x-40 gap-y-10 md:grid-cols-2 md:gap-y-0">
+        <div className="z-40 mx-40 grid gap-x-40 gap-y-10 md:mx-20 md:grid-cols-2 md:gap-y-0">
           <div className="flex flex-col items-center justify-center">
             <h2 className="mb-5 text-lg font-bold">Spiral of Decay T-shirt</h2>
             <img
-              className="max-w-[350px] lg:max-w-[450px]"
+              className="max-w-[250px] sm:max-w-[350px] lg:max-w-[450px]"
               src="static/images/single-print.jpg"
               alt="fos single print"
             />
@@ -20,7 +20,7 @@ const Merch = () => {
             <div title="Click to see the back" onClick={() => toggleFront(!showFront)}>
               <img
                 style={{ display: !showFront ? 'block' : 'none' }}
-                className="max-w-[350px] cursor-pointer lg:max-w-[450px]"
+                className="max-w-[250px] cursor-pointer sm:max-w-[350px] lg:max-w-[450px]"
                 src="static/images/double-print-front.jpg"
                 alt="fos double printfront"
               />
